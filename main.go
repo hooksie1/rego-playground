c1d879aa9ed6ac2678e19b5685bcb6a08f0618e8
John Hooks <john.hooks@chainguard.dev>
2025-05-01 14:37:41 -0400

    more updates

--- main.go
+++ main.go
@@ -1,164 +1,24 @@
 package main
 
 import (
-	"bytes"
-	"compress/flate"
-	"context"
-	_ "embed"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"io"
-	"syscall/js"
-
-	"github.com/open-policy-agent/opa/rego"
-	"github.com/open-policy-agent/opa/storage/inmem"
-	"github.com/open-policy-agent/opa/util"
-	//"github.com/tylermmorton/tmpl"
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
 )
 
-//func things() {
-//
-//	a := "policy"
-//	b := base64.URLEncoding.EncodeToString([]byte(a))
-//	buff := bytes.NewBuffer([]byte(b))
-//
-//	bout := &bytes.Buffer{}
-//	zwtr, err := flate.NewWriter(bout, flate.BestCompression)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	_, err = io.Copy(zwtr, buff)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	zwtr.Close()
-//	new := base64.URLEncoding.EncodeToString(bout.Bytes())
-//	fmt.Println(new)
-//
-//	old, err := base64.URLEncoding.DecodeString(new)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//	bin := bytes.NewBuffer(old)
-//	zrdr := flate.NewReader(bin)
-//	io.Copy(os.Stdout, zrdr)
-//}
-
-func compressWrapper() js.Func {
-	return js.FuncOf(func(this js.Value, args []js.Value) any {
-		if len(args) == 0 {
-			return "Argument required"
-		}
-		data := args[0].String()
-		b := base64.URLEncoding.EncodeToString([]byte(data))
-		buff := bytes.NewBuffer([]byte(b))
-
-		bout := &bytes.Buffer{}
-		zwtr, err := flate.NewWriter(bout, flate.BestCompression)
-		if err != nil {
-			return err
-		}
-		_, err = io.Copy(zwtr, buff)
-		if err != nil {
-			return err
-		}
-
-		zwtr.Close()
-		resp := base64.URLEncoding.EncodeToString(bout.Bytes())
-		return resp
-	})
-}
-
-func decompressWrapper() js.Func {
-	return js.FuncOf(func(this js.Value, args []js.Value) any {
-		if len(args) == 0 {
-			return "Argument required"
-		}
-		data := args[0].String()
-		old, err := base64.URLEncoding.DecodeString(data)
-		if err != nil {
-			return err.Error()
-		}
-		bin := bytes.NewBuffer(old)
-		zrdr := flate.NewReader(bin)
-		buf := bytes.Buffer{}
-		_, err = io.Copy(&buf, zrdr)
-		if err != nil {
-			return err.Error()
-		}
-		l, err := base64.URLEncoding.DecodeString(buf.String())
-		if err != nil {
-			return err.Error()
-		}
-		return string(l)
-	})
-}
-
-func evalRego(input, data, pkg string) (string, error) {
-	var in any
-	d := json.NewDecoder(bytes.NewBufferString(input))
-	d.UseNumber()
-	if err := d.Decode(&in); err != nil {
-		return "", err
-	}
-	rg := rego.New(
-		rego.Query("data.play"),
-		rego.Module("play.rego", pkg),
-		rego.Input(in),
-	)
-	if data != "" {
-		var jdata map[string]any
-
-		err := util.UnmarshalJSON([]byte(data), &jdata)
-		if err != nil {
-			return "", err
-		}
-		store := inmem.NewFromObject(jdata)
-		f := rego.Store(store)
-		f(rg)
-
-	}
-
-	ctx := context.Background()
-	rs, err := rg.Eval(ctx)
-	if err != nil {
-		return "", err
-	}
-	if len(rs) == 0 {
-		return "", err
-	}
-
-	m, err := json.Marshal(rs[0].Expressions[0].Value)
-	if err != nil {
-		return "", err
-	}
-	return string(m), nil
-}
-
-func regoWrapper() js.Func {
-	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
-		if len(args) < 3 {
-			return "Invalid no arguments passed"
-		}
-		inputJSON := args[0].String()
-		dataJSON := args[1].String()
-		pkg := args[2].String()
-		pretty, err := evalRego(inputJSON, dataJSON, pkg)
-		if err != nil {
-			fmt.Printf("unable to convert to json %s\n", err)
-			return err.Error()
-		}
-		return pretty
-	})
-	return jsonFunc
-}
+//go:embed static/*
+var static embed.FS
 
 func main() {
-	js.Global().Set("evalRego", regoWrapper())
-	js.Global().Set("decompressData", decompressWrapper())
-	js.Global().Set("compressData", compressWrapper())
-	fmt.Println("playground loaded")
-	<-make(chan struct{})
+	r := http.NewServeMux()
+
+	content, err := fs.Sub(static, "static")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r.Handle("GET /", http.FileServer(http.FS(content)))
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
---
+++ wasm/main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bytes"
+	"compress/flate"
+	"context"
+	_ "embed"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io"
+	"syscall/js"
+
+	"github.com/open-policy-agent/opa/rego"
+	"github.com/open-policy-agent/opa/storage/inmem"
+	"github.com/open-policy-agent/opa/util"
+	//"github.com/tylermmorton/tmpl"
+)
+
+func compressWrapper() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) == 0 {
+			return "Argument required"
+		}
+		data := args[0].String()
+		b := base64.URLEncoding.EncodeToString([]byte(data))
+		buff := bytes.NewBuffer([]byte(b))
+
+		bout := &bytes.Buffer{}
+		zwtr, err := flate.NewWriter(bout, flate.BestCompression)
+		if err != nil {
+			return err
+		}
+		_, err = io.Copy(zwtr, buff)
+		if err != nil {
+			return err
+		}
+
+		zwtr.Close()
+		resp := base64.URLEncoding.EncodeToString(bout.Bytes())
+		return resp
+	})
+}
+
+func decompressWrapper() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) == 0 {
+			return "Argument required"
+		}
+		data := args[0].String()
+		old, err := base64.URLEncoding.DecodeString(data)
+		if err != nil {
+			return err.Error()
+		}
+		bin := bytes.NewBuffer(old)
+		zrdr := flate.NewReader(bin)
+		buf := bytes.Buffer{}
+		_, err = io.Copy(&buf, zrdr)
+		if err != nil {
+			return err.Error()
+		}
+		l, err := base64.URLEncoding.DecodeString(buf.String())
+		if err != nil {
+			return err.Error()
+		}
+		return string(l)
+	})
+}
+
+func evalRego(input, data, pkg string) (string, error) {
+	var in any
+	d := json.NewDecoder(bytes.NewBufferString(input))
+	d.UseNumber()
+	if err := d.Decode(&in); err != nil {
+		return "", err
+	}
+	rg := rego.New(
+		rego.Query("data.play"),
+		rego.Module("play.rego", pkg),
+		rego.Input(in),
+	)
+	if data != "" {
+		var jdata map[string]any
+
+		err := util.UnmarshalJSON([]byte(data), &jdata)
+		if err != nil {
+			return "", err
+		}
+		store := inmem.NewFromObject(jdata)
+		f := rego.Store(store)
+		f(rg)
+
+	}
+
+	ctx := context.Background()
+	rs, err := rg.Eval(ctx)
+	if err != nil {
+		return "", err
+	}
+	if len(rs) == 0 {
+		return "", err
+	}
+
+	m, err := json.Marshal(rs[0].Expressions[0].Value)
+	if err != nil {
+		return "", err
+	}
+	return string(m), nil
+}
+
+func regoWrapper() js.Func {
+	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) any {
+		if len(args) < 3 {
+			return "Invalid no arguments passed"
+		}
+		inputJSON := args[0].String()
+		dataJSON := args[1].String()
+		pkg := args[2].String()
+		pretty, err := evalRego(inputJSON, dataJSON, pkg)
+		if err != nil {
+			fmt.Printf("unable to convert to json %s\n", err)
+			return err.Error()
+		}
+		return pretty
+	})
+	return jsonFunc
+}
+
+func main() {
+	js.Global().Set("evalRego", regoWrapper())
+	js.Global().Set("decompressData", decompressWrapper())
+	js.Global().Set("compressData", compressWrapper())
+	fmt.Println("playground loaded")
+	<-make(chan struct{})
+}
