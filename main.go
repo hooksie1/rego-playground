2b578a7da6ad91969e56439f8918606a7c7d889a
John Hooks <hooksie11@gmail.com>
2026-08-14 15:10:50 -0400

    add foo

--- main.go
+++ main.go
@@ -15,7 +15,9 @@ import (
 type Request struct {
 	Input   string `json:"input"`
 	Data    string `json:"data"`
-	Package string `json:"package"`}
+	Foo     string `json:"whoa"`
+	Package string `json:"package"`
+}
 
 func playgrounds(w http.ResponseWriter, r *http.Request) {
 	// something here
@@ -69,7 +71,7 @@ func playgrounds(w http.ResponseWriter, r *http.Request) {
 func main() {
 	r := http.NewServeMux()
 
-	r.Handle("/whoa", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){w.Write([]byte("Hello"))}))
+	r.Handle("/whoa", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("Hello")) }))
 	r.Handle("POST /playgrounds", http.HandlerFunc(playgrounds))
 	r.Handle("/", http.FileServer(http.Dir("static")))
 	r.Handle("/test", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { w.Write([]byte("hey")) }))
